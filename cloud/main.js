a58e2711ae8a9cd3ff9964cf53cb2f07c0956ef1
arata-1972 <arata1972.info@gmail.com>
2016-10-18 05:53:04 +0900

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -9,4 +9,6 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 
-
+Parse.Cloud.define('elementsBaidu', function(req, res) {
+ res.success('document.getElementsByClassName(@ordinary-output target-output clearfix@)[0].innerText');
+});

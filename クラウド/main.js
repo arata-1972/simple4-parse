f18fa085d713a6874f648a10a0b61fbc896c7021
arata-1972 <arata1972.info@gmail.com>
2016-10-26 02:20:40 +0900

    Update and rename cloud/main.js to クラウド/main.js

--- クラウド/main.js
+++ クラウド/main.js
@@ -12,3 +12,21 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define('elementsBaidu', function(req, res) {
  res.success('document.getElementsByClassName(<ordinary-output target-output clearfix>)[0].innerText');
 });
+
+
+Parse.Push.send({
+  where: query,
+  data: {
+    alert: 'Test',
+    badge: 1,
+    sound: 'default'
+  }
+}, {
+  useMasterKey: true,
+  success: function() {
+    // Push sent!
+  },
+  error: function(error) {
+    // There was a problem :(
+  }
+});

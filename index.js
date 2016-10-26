ba357157529ee95d84d79fcab00459a15c4d8e09
arata-1972 <arata1972.info@gmail.com>
2016-10-26 17:29:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var api = new ParseServer({
     //   apiKey: '...'
     // },
     ios: {
-      pfx: '/iPhone12/com.satoshogoTranslate-Chinese store.p12', // 與 index.js 目錄同層
+      pfx:  __dirname + '/iPhone12/com.satoshogoTranslate-Chinese store.p12', // 與 index.js 目錄同層
       bundleId: 'com.satoshogoTranslate-Chinese', // 填入先前填的 Bundle ID
       production: true // false: development, true: production
     }

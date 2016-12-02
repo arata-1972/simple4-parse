859131a1fae0b36d4a800206e0b74703230585b3
arata-1972 <arata1972.info@gmail.com>
2016-12-02 10:08:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -83,9 +83,14 @@ var api = new ParseServer({
 	        bundleId: 'com.satoshogoKoreanRussian',  
 	        production: true
 	      },
-		    {
+		{
 	        pfx: __dirname + '/iPhoneP12/com.satoshogoKoreanVietnamnese store.p12', 
 	        bundleId: 'com.satoshogoKoreanVietnamnese',  
+	        production: true
+	      },
+		     {
+	        pfx: __dirname + '/iPhoneP12/com.satoshogoChineseArabicEglish store.p12', 
+	        bundleId: 'com.satoshogoChineseArabicEglish',  
 	        production: true
 	      },
 		{

fc538769dbf2252b8c47ec8d9fa491cb41bb0cb8
arata-1972 <arata1972.info@gmail.com>
2017-01-05 13:02:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-	  databaseURI: databaseUri || 'mongodb://heroku_17wmt6f9:ac3vq8po8glc39sgmimc45ljhf@ds031995.mlab.com:31995/heroku_17wmt6f9',
+	  databaseURI: databaseUri || 'mongodb://heroku_p3jb2brn:adddjara7jgmdoq20lo5rh6cbr@ds017185.mlab.com:17185/heroku_p3jb2brn',
 	  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-	  appId: process.env.APP_ID || '2B5WHg0N2AZi5kQ5cM85Mm8dV9BVKWSAsz31naC3',
-	  masterKey: process.env.MASTER_KEY || 'UsK236qFsEyFXN7pLReWdMbs1EdduqdGlE2pfsT6', //Add your master key here. Keep it secret!
-	  serverURL: process.env.SERVER_URL || 'https://english-persian.herokuapp.com/parse',  // Don't forget to change to https if needed
+	  appId: process.env.APP_ID || 'ek8gqIAkIA3pb2SSRKYjt7yjp1ETdNoxFxMxqM58',
+	  masterKey: process.env.MASTER_KEY || 'viyCQynnuDfWgzYefBsZYTb64tFry34AQUgh2jIh', //Add your master key here. Keep it secret!
+	  serverURL: process.env.SERVER_URL || 'https://simple-translator1.herokuapp.com/parse',  // Don't forget to change to https if needed
 	  liveQuery: {
 	    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
 	  },
@@ -33,8 +33,28 @@ var api = new ParseServer({
 	        bundleId: 'com.satoshogo.EnglishPersian',
 	        production: true
 	      },
+		    {
+	        pfx: __dirname + '/iPhoneP12/com.satoshogo.EnglishPersian store.p12',
+	        bundleId: 'com.satoshogo.EnglishPersian',
+	        production: true
+	      },
+		    {
+	        pfx: __dirname + '/iPhoneP12/com.satoshogo.EnglishPersian store.p12',
+	        bundleId: 'com.satoshogo.EnglishPersian',
+	        production: true
+	      },
+		    {
+	        pfx: __dirname + '/iPhoneP12/com.satoshogo.EnglishPersian store.p12',
+	        bundleId: 'com.satoshogo.EnglishPersian',
+	        production: true
+	      },
+		    {
+	        pfx: __dirname + '/iPhoneP12/com.satoshogo.EnglishPersian store.p12',
+	        bundleId: 'com.satoshogo.EnglishPersian',
+	        production: true
+	      },
 	  {
-	        pfx: __dirname + '/iPhoneP12/com.satoshogo.EnglishPersian.remake store.p12', 
+		pfx: __dirname + '/iPhoneP12/com.satoshogo.EnglishPersian.remake store.p12', 
 	        bundleId: 'com.satoshogo.EnglishPersian.remake',  
 	        production: true
 	      }
